f9550166f9cdc17b0c6f1b3514167ff8a16b52fb
ApsanaMohammad <apsanamohammad777@gmail.com>
2024-12-12 21:11:48 +0530

    initial commit

--- MentorshipPlatformAPI/MentorshipPlatformAPI/obj/Debug/net6.0/MentorshipPlatformAPI.AssemblyInfo.cs
+++ MentorshipPlatformAPI/MentorshipPlatformAPI/obj/Debug/net6.0/MentorshipPlatformAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MentorshipPlatformAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d2d722b681ef5e14833d05dc19d9d47d5760ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MentorshipPlatformAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MentorshipPlatformAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
